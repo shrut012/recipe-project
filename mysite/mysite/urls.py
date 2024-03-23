c86a7fb47fdef0ac59bf3f9adf017e5ff2b9d1af
Shrut <shrutpansuriya@gmail.com>
2024-03-23 13:01:29 +0530

    added get and delete routes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from vege.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('recipes/', recipes, name='recipes'),
+    path('recipes/', add_recipe, name='add_recipe'),
+    path('delete-recipe/<id>/', delete_recipe, name='delete_recipe'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
--- mysite/vege/views.py
+++ mysite/vege/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Recipe
 
 
-def recipes(request):
-    return render(request, 'recipes.html')
+def add_recipe(request):
+    if request.method == 'POST':
+        recipe_name = request.POST.get('recipe_name')
+        recipe_description = request.POST.get('recipe_description')
+        recipe_image = request.FILES.get('recipe_image')
+        # print("Recipe Name:", recipe_name)
+        # print("Recipe Description:", recipe_description)
+        # print("Recipe Image:", recipe_image)
+
+        Recipe.objects.create(
+            recipe_name = recipe_name,
+            recipe_description = recipe_description,
+            recipe_image = recipe_image
+        )
+
+        return redirect('/recipes')
+
+    queryset = Recipe.objects.all()
+    context = {'page': 'Add Recipe', 'recipes': queryset}
+    return render(request, 'add_recipe.html', context)
+
+def delete_recipe(request, id):
+    print("called")
+    queryset = Recipe.objects.get(id= id)
+    queryset.delete()
+    return redirect('/recipes')
